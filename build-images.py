61d1c6bbc28483657f27508563ae1d2e5052b3b6
Adam Hill <adam@diginc.us>
2020-05-06 21:29:25 -0500

    Build images from secret webhook urls
    Signed-off-by: Adam Hill <adam@diginc.us>

---
+++ build-images.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+import requests
+from os import environ
+
+
+REPOS = [
+    'diginc/pipenv-goodies',
+    'diginc/debian-terraform-aws'
+]
+
+
+def main():
+    for repo, repo_build_url in REPOS.items():
+        build_url_key = repo.replace('/', '_').replace('-', '_')
+        if environ.get(build_url_key) is not None:
+            requests.post(environ.get(build_url_key))
+        else:
+            print("No build URL environment variable found:", build_url_key)
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
